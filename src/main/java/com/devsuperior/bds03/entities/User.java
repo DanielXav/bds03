1b5e6c1b73c871bfadc9448172945c9a16afd29c
Daniel Xavier <dxavier194@gmail.com>
2022-04-07 15:17:40 -0300

    Interfaces Spring Security

--- src/main/java/com/devsuperior/bds03/entities/User.java
+++ src/main/java/com/devsuperior/bds03/entities/User.java
@@ -16,10 +16,11 @@ import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
-@Table(name = "tb_users")
+@Table(name = "tb_user")
 public class User implements UserDetails {
 
 	@Id
@@ -84,37 +85,31 @@ public class User implements UserDetails {
 
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		// TODO Auto-generated method stub
-		return null;
+		return roles.stream().map(x -> new SimpleGrantedAuthority(x.getAuthority())).toList();
 	}
 
 	@Override
 	public String getUsername() {
-		// TODO Auto-generated method stub
-		return null;
+		return email;
 	}
 
 	@Override
 	public boolean isAccountNonExpired() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
 	public boolean isAccountNonLocked() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
 	public boolean isCredentialsNonExpired() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
 	public boolean isEnabled() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 	
